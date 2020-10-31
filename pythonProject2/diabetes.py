6df5496ab6a1eddc57eb03b59165e47eec204ece
Sakshi Khochare <sakshi.khochare@gmail.com>
2020-10-31 16:33:20 +0530

    diabetes

--- pythonProject2/diabetes.py
+++ pythonProject2/diabetes.py
@@ -1,136 +1,53 @@
+#Importing Libraries
 import pandas as pd
-import numpy as np
 
-
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.model_selection import  GridSearchCV, RandomizedSearchCV
-import lightgbm as lgbm
-from sklearn.ensemble import VotingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from yellowbrick.classifier import DiscriminationThreshold
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 import pickle
 
+#Data Loading
+df=pd.read_csv('D:/Github/ML-Health-Status-Detection-Web-App/pythonProject2/diabetes.csv')
+df_name=df.columns
 
-from scipy.stats import randint as sp_randint
-from scipy.stats import uniform as sp_uniform
+#Replacing null values
+df['Glucose'].fillna(df['Glucose'].mean(), inplace = True)
+df['BloodPressure'].fillna(df['BloodPressure'].mean(), inplace = True)
+df['SkinThickness'].fillna(df['SkinThickness'].median(), inplace = True)
+df['Insulin'].fillna(df['Insulin'].median(), inplace = True)
+df['BMI'].fillna(df['BMI'].median(), inplace = True)
 
-data=pd.read_csv('C:/Users/saksh/PycharmProjects/pythonProject2/diabetes.csv')
-data[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = data[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
-def median_target(var):
-    temp = data[data[var].notnull()]
-    temp = temp[[var, 'Outcome']].groupby(['Outcome'])[[var]].median().reset_index()
-    return temp
+#Data Spliting
+X=df[df_name[0:8]]
+Y=df[df_name[8]]
 
-data.loc[(data['Outcome'] == 0 ) & (data['Insulin'].isnull()), 'Insulin'] = 102.5
-data.loc[(data['Outcome'] == 1 ) & (data['Insulin'].isnull()), 'Insulin'] = 169.5
-data.loc[(data['Outcome'] == 0 ) & (data['Glucose'].isnull()), 'Glucose'] = 107
-data.loc[(data['Outcome'] == 1 ) & (data['Glucose'].isnull()), 'Glucose'] = 140
-data.loc[(data['Outcome'] == 0 ) & (data['SkinThickness'].isnull()), 'SkinThickness'] = 27
-data.loc[(data['Outcome'] == 1 ) & (data['SkinThickness'].isnull()), 'SkinThickness'] = 32
-data.loc[(data['Outcome'] == 0 ) & (data['BloodPressure'].isnull()), 'BloodPressure'] = 70
-data.loc[(data['Outcome'] == 1 ) & (data['BloodPressure'].isnull()), 'BloodPressure'] = 74.5
-data.loc[(data['Outcome'] == 0 ) & (data['BMI'].isnull()), 'BMI'] = 30.1
-data.loc[(data['Outcome'] == 1 ) & (data['BMI'].isnull()), 'BMI'] = 34.3
 
-target_col = ["Outcome"]
-cat_cols = data.nunique()[data.nunique() < 12].keys().tolist()
-cat_cols = [x for x in cat_cols]
-# numerical columns
-num_cols = [x for x in data.columns if x not in cat_cols + target_col]
-# Binary columns with 2 values
-bin_cols = data.nunique()[data.nunique() == 2].keys().tolist()
-# Columns more than 2 values
-multi_cols = [i for i in cat_cols if i not in bin_cols]
+#Split data into train and test data
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=0,stratify=Y)
 
-# Label encoding Binary columns
-le = LabelEncoder()
-for i in bin_cols:
-    data[i] = le.fit_transform(data[i])
+#Resampling-downSampling
+X = pd.concat([X_train, Y_train], axis=1)
+not_diabetic = X[X.Outcome==0]
+diabetic = X[X.Outcome==1]
+from sklearn.utils import resample
+not_diabetic_downsampled = resample(not_diabetic,
+                          replace=True, # sample with replacement
+                          n_samples=len(diabetic), # match number in majority class
+                          random_state=27)
+downsampled = pd.concat([not_diabetic_downsampled,diabetic])
+Y_train = downsampled.Outcome
+X_train = downsampled.drop('Outcome', axis=1)
 
-# Duplicating columns for multi value columns
-data = pd.get_dummies(data=data, columns=multi_cols)
+RandomForestClassifier=RandomForestClassifier(random_state = 1,n_estimators= 1000,
+ min_samples_split= 5,
+ min_samples_leaf=2,
+ max_features= 'sqrt',
+ max_depth= 10,
+ bootstrap= True
+ )
+RandomForestClassifier.fit(X_train,Y_train)
 
-# Scaling Numerical columns
-std = StandardScaler()
-scaled = std.fit_transform(data[num_cols])
-scaled = pd.DataFrame(scaled, columns=num_cols)
 
-# dropping original values merging scaled values for numerical columns
-df_data_og = data.copy()
-data = data.drop(columns=num_cols, axis=1)
-data = data.merge(scaled, left_index=True, right_index=True, how="left")
-
-#Splitting Data
-X = data.drop('Outcome', 1)
-y = data['Outcome']
-
-random_state=42
-
-fit_params = {"early_stopping_rounds" : 100,
-             "eval_metric" : 'auc',
-             "eval_set" : [(X,y)],
-             'eval_names': ['valid'],
-             'verbose': 0,
-             'categorical_feature': 'auto'}
-
-param_test = {'learning_rate' : [0.01, 0.02, 0.03, 0.04, 0.05, 0.08, 0.1, 0.2, 0.3, 0.4],
-              'n_estimators' : [100, 200, 300, 400, 500, 600, 800, 1000, 1500, 2000],
-              'num_leaves': sp_randint(6, 50),
-              'min_child_samples': sp_randint(100, 500),
-              'min_child_weight': [1e-5, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4],
-              'subsample': sp_uniform(loc=0.2, scale=0.8),
-              'max_depth': [-1, 1, 2, 3, 4, 5, 6, 7],
-              'colsample_bytree': sp_uniform(loc=0.4, scale=0.6),
-              'reg_alpha': [0, 1e-1, 1, 2, 5, 7, 10, 50, 100],
-              'reg_lambda': [0, 1e-1, 1, 5, 10, 20, 50, 100]}
-
-#number of combinations
-n_iter = 300
-
-#intialize lgbm and lunch the search
-lgbm_clf = lgbm.LGBMClassifier(random_state=random_state, silent=True, metric='None', n_jobs=4)
-grid_search = RandomizedSearchCV(
-    estimator=lgbm_clf, param_distributions=param_test,
-    n_iter=n_iter,
-    scoring='accuracy',
-    cv=5,
-    refit=True,
-    random_state=random_state,
-    verbose=True)
-
-grid_search.fit(X, y, **fit_params)
-opt_parameters =  grid_search.best_params_
-lgbm_clf = lgbm.LGBMClassifier(**opt_parameters)
-
-visualizer = DiscriminationThreshold(lgbm_clf)
-
-visualizer.fit(X, y)
-visualizer.poof()
-
-knn_clf = KNeighborsClassifier()
-
-voting_clf = VotingClassifier(estimators=[
-    ('lgbm_clf', lgbm_clf),
-    ('knn', KNeighborsClassifier())], voting='soft', weights=[1, 1])
-
-params = {
-    'knn__n_neighbors': np.arange(1, 30)
-}
-
-grid = GridSearchCV(estimator=voting_clf, param_grid=params, cv=5)
-
-grid.fit(X, y)
-
-knn_clf = KNeighborsClassifier(n_neighbors = 25)
-
-voting_clf = VotingClassifier (
-        estimators = [('knn', knn_clf), ('lgbm', lgbm_clf)],
-                     voting='soft', weights = [1,1])
-
-visualizer = DiscriminationThreshold(voting_clf)
-
-visualizer.fit(X, y)
-visualizer.poof()
 
 filename = 'diabetes.sav'
-pickle.dump(visualizer, open(filename, 'wb'))
+pickle.dump(RandomForestClassifier, open(filename, 'wb'))
