9f6aabe1c11c6d2ce5103de263034bb44ed02068
Sakshi Khochare <sakshi.khochare@gmail.com>
2020-11-04 01:45:04 +0530

    Ml

--- pythonProject2/diabetes.py
+++ pythonProject2/diabetes.py
@@ -1,9 +1,12 @@
 #Importing Libraries
+import numpy as np
 import pandas as pd
 
-from sklearn.metrics import accuracy_score
+
+
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.utils import resample
 import pickle
 
 #Data Loading
@@ -17,6 +20,14 @@ df['SkinThickness'].fillna(df['SkinThickness'].median(), inplace = True)
 df['Insulin'].fillna(df['Insulin'].median(), inplace = True)
 df['BMI'].fillna(df['BMI'].median(), inplace = True)
 
+
+#Resampling-UpSampling
+minority = df[df.Outcome==1]
+majority = df[df.Outcome==0]
+
+minority_upsample = resample(minority, replace=True, n_samples=majority.shape[0])
+df = pd.concat([minority_upsample, majority], axis=0)
+
 #Data Spliting
 X=df[df_name[0:8]]
 Y=df[df_name[8]]
@@ -25,29 +36,11 @@ Y=df[df_name[8]]
 #Split data into train and test data
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=0,stratify=Y)
 
-#Resampling-downSampling
-X = pd.concat([X_train, Y_train], axis=1)
-not_diabetic = X[X.Outcome==0]
-diabetic = X[X.Outcome==1]
-from sklearn.utils import resample
-not_diabetic_downsampled = resample(not_diabetic,
-                          replace=True, # sample with replacement
-                          n_samples=len(diabetic), # match number in majority class
-                          random_state=27)
-downsampled = pd.concat([not_diabetic_downsampled,diabetic])
-Y_train = downsampled.Outcome
-X_train = downsampled.drop('Outcome', axis=1)
 
-RandomForestClassifier=RandomForestClassifier(random_state = 1,n_estimators= 1000,
- min_samples_split= 5,
- min_samples_leaf=2,
- max_features= 'sqrt',
- max_depth= 10,
- bootstrap= True
- )
-RandomForestClassifier.fit(X_train,Y_train)
 
+model = ExtraTreesClassifier(random_state=123)
+model.fit(X_train, Y_train)
 
 
 filename = 'diabetes.sav'
-pickle.dump(RandomForestClassifier, open(filename, 'wb'))
+pickle.dump(model, open(filename, 'wb'))
