03bb04d2f6ed6002db5112b6931e7d4f2f544c23
Sakshi Khochare <sakshi.khochare@gmail.com>
2020-11-18 15:59:49 +0530

    Update Pneumonia.py

--- pythonProject2/Pneumonia.py
+++ pythonProject2/Pneumonia.py
@@ -121,6 +121,39 @@ hist = model.fit_generator(
            epochs=epochs, validation_data=test_gen, 
            validation_steps=test_gen.samples // batch_size)
 
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+ax = ax.ravel()
+
+for i, met in enumerate(['acc', 'loss']):
+    ax[i].plot(hist.history[met])
+    ax[i].plot(hist.history['val_' + met])
+    ax[i].set_title('Model {}'.format(met))
+    ax[i].set_xlabel('epochs')
+    ax[i].set_ylabel(met)
+    ax[i].legend(['train', 'val'])
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+preds = model.predict(test_data)
+
+acc = accuracy_score(test_labels, np.round(preds))*100
+cm = confusion_matrix(test_labels, np.round(preds))
+tn, fp, fn, tp = cm.ravel()
+
+print('CONFUSION MATRIX ------------------')
+print(cm)
+
+print('\nTEST METRICS ----------------------')
+precision = tp/(tp+fp)*100
+recall = tp/(tp+fn)*100
+print('Accuracy: {}%'.format(acc))
+print('Precision: {}%'.format(precision))
+print('Recall: {}%'.format(recall))
+print('F1-score: {}'.format(2*precision*recall/(precision+recall)))
+
+print('\nTRAIN METRIC ----------------------')
+print('Train acc: {}'.format(np.round((hist.history['acc'][-1])*100, 2)))
+
 test_data = []
 img = plt.imread('E:\Projects\ML\Datasets\chest_xray\test\PNEUMONIA\person1_virus_6.jpeg')
 img = cv2.resize(img, (img_dims, img_dims))
